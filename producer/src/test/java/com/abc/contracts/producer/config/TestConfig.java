8bfd7f336fa1bdcf922c895b1685bc4a7a9b0c4a
Alhagie Bai Cham <cham207388@gmail.com>
2025-01-10 13:04:45 -0600

    update the BaseContractTest class. @MockBean is deprecated

---
+++ producer/src/test/java/com/abc/contracts/producer/config/TestConfig.java
@@ -0,0 +1,23 @@
+package com.abc.contracts.producer.config;
+
+import static org.mockito.Mockito.mock;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.abc.contracts.producer.controllers.PostController;
+import com.abc.contracts.producer.services.PostService;
+
+@Configuration
+public class TestConfig {
+
+  @Bean
+  public PostService postService() {
+    return mock(PostService.class); // Provide a mocked PostService
+  }
+
+  @Bean
+  public PostController postController(PostService postService) {
+    return new PostController(postService); // Provide PostController using mocked PostService
+  }
+}
