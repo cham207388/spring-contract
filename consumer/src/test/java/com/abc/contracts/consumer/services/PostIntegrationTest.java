0cd28c4427f8702f1dbea23063f038e176085716
Alhagie Bai Cham <cham207388@gmail.com>
2025-01-12 18:30:38 -0600

    autowired

--- consumer/src/test/java/com/abc/contracts/consumer/services/PostIntegrationTest.java
+++ consumer/src/test/java/com/abc/contracts/consumer/services/PostIntegrationTest.java
@@ -1,16 +1,16 @@
 package com.abc.contracts.consumer.services;
 
-import com.abc.contracts.consumer.client.RestfulClient;
-import com.abc.contracts.consumer.domains.Post;
-import com.abc.contracts.consumer.domains.PostResponse;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.test.context.ActiveProfiles;
 
-
-import static org.assertj.core.api.Assertions.assertThat;
+import com.abc.contracts.consumer.client.RestfulClient;
+import com.abc.contracts.consumer.domains.Post;
+import com.abc.contracts.consumer.domains.PostResponse;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
@@ -23,6 +23,7 @@ class PostIntegrationTest {
 
     private final RestfulClient restfulClient;
     
+    @Autowired
     public PostIntegrationTest(RestfulClient restfulClient) {
         this.restfulClient = restfulClient;
     }
