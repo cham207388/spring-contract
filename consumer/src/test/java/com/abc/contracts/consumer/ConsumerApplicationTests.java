db5a0089e1532e34dc9c50e2c10dd4a0ce685125
Alhagie Bai Cham <cham207388@gmail.com>
2025-01-16 14:04:31 -0600

    test context

--- consumer/src/test/java/com/abc/contracts/consumer/ConsumerApplicationTests.java
+++ consumer/src/test/java/com/abc/contracts/consumer/ConsumerApplicationTests.java
@@ -1,5 +1,6 @@
 package com.abc.contracts.consumer;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
@@ -9,7 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 class ConsumerApplicationTests {
 
-// @Test
+    @Test
     void contextLoads() {
     }
 
