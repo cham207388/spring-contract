98d8a1ae6573053ab210fa77cf6e79c296ba3184
Alhagie Bai Cham <cham207388@gmail.com>
2025-01-16 14:25:31 -0600

    no test for context

--- consumer/src/test/java/com/abc/contracts/consumer/ConsumerApplicationTests.java
+++ consumer/src/test/java/com/abc/contracts/consumer/ConsumerApplicationTests.java
@@ -1,28 +1,14 @@
 package com.abc.contracts.consumer;
 
-import javax.sql.DataSource;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-
-import jakarta.persistence.EntityManagerFactory;
 
 @SpringBootTest
 // @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-@EnableAutoConfiguration(exclude = {HibernateJpaAutoConfiguration.class})
 @ActiveProfiles("test")
 class ConsumerApplicationTests {
 
-    @MockitoBean
-    private DataSource dataSource;
-    @MockitoBean
-    private EntityManagerFactory entityManagerFactory;
-
-    @Test
+    // @Test
     void contextLoads() {
     }
 
