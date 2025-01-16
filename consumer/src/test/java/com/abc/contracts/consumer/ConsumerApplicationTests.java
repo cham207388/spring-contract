ccb6ffe4dac26c7fc38b7d38755f0d38ce4bb7c9
Alhagie Bai Cham <cham207388@gmail.com>
2025-01-16 14:18:30 -0600

    pipeline

--- consumer/src/test/java/com/abc/contracts/consumer/ConsumerApplicationTests.java
+++ consumer/src/test/java/com/abc/contracts/consumer/ConsumerApplicationTests.java
@@ -1,11 +1,19 @@
 package com.abc.contracts.consumer;
 
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 // @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@ActiveProfiles("test")
 class ConsumerApplicationTests {
+
+    @MockitoBean
+    private DataSource dataSource;
 
     @Test
     void contextLoads() {
