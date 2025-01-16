a81b75286c996f176524be666f71b94f8bf03389
Alhagie Bai Cham <cham207388@gmail.com>
2025-01-16 14:23:39 -0600

    test context

--- consumer/src/test/java/com/abc/contracts/consumer/ConsumerApplicationTests.java
+++ consumer/src/test/java/com/abc/contracts/consumer/ConsumerApplicationTests.java
@@ -3,16 +3,24 @@ package com.abc.contracts.consumer;
 import javax.sql.DataSource;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import jakarta.persistence.EntityManagerFactory;
 
 @SpringBootTest
 // @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-// @ActiveProfiles("test")
+@EnableAutoConfiguration(exclude = {HibernateJpaAutoConfiguration.class})
+@ActiveProfiles("test")
 class ConsumerApplicationTests {
 
     @MockitoBean
     private DataSource dataSource;
+    @MockitoBean
+    private EntityManagerFactory entityManagerFactory;
 
     @Test
     void contextLoads() {
