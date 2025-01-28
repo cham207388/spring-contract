534fbb854c26a31789e850d343a7eff7d69afb82
Alhagie Bai Cham <cham207388@gmail.com>
2025-01-28 10:36:28 -0600

    listener received and failed

--- producer/src/main/java/com/abc/contracts/producer/config/ProducerRabbitMQConfig.java
+++ producer/src/main/java/com/abc/contracts/producer/config/ProducerRabbitMQConfig.java
@@ -1,16 +1,27 @@
 package com.abc.contracts.producer.config;
+
 import com.abc.contracts.producer.messaging.PostMessageListener;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ProducerRabbitMQConfig {
+
+    @Value("${rabbitmq.post.queue}")
+    private String postQueue;
+
+    @Bean
+    public Queue postQueue() {
+        return new Queue(postQueue, true);
+    }
 
     @Bean
     public Jackson2JsonMessageConverter jackson2JsonMessageConverter(ObjectMapper objectMapper) {
