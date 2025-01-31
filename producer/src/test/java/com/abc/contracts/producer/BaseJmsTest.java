b95cca23b93af5b670005135af24d0a03adc2873
Alhagie Bai Cham <cham207388@gmail.com>
2025-01-31 17:39:35 -0600

    regular test manually is passing

--- producer/src/test/java/com/abc/contracts/producer/BaseJmsTest.java
+++ producer/src/test/java/com/abc/contracts/producer/BaseJmsTest.java
@@ -1,17 +1,14 @@
 package com.abc.contracts.producer;
 
-import com.abc.contracts.producer.config.EmbeddedArtemisInitializer;
 import com.abc.contracts.producer.config.EmbeddedArtemisTestConfig;
 import com.abc.contracts.producer.config.JmsTestConfig;
 import org.apache.activemq.artemis.core.server.embedded.EmbeddedActiveMQ;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -20,28 +17,17 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @ContextConfiguration(classes = {JmsTestConfig.class, EmbeddedArtemisTestConfig.class})
 @AutoConfigureMessageVerifier
 @Testcontainers
-@ActiveProfiles("test") // Activate the "test" profile
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public abstract class BaseJmsTest {
 
     @Autowired
     protected JmsTemplate jmsTemplate;
 
-//    @BeforeAll
-//    public static void startBroker() throws Exception {
-//        EmbeddedActiveMQ embeddedActiveMQ = new EmbeddedArtemisTestConfig().embeddedActiveMQ();
-//        System.out.println("Broker started successfully!");
-//        embeddedActiveMQ.start();
-//    }
-
     @BeforeAll
-    public static void startBroker() {
-        // The broker is already started by EmbeddedArtemisInitializer
-    }
-
-    @AfterAll
-    public static void stopBroker() {
-        EmbeddedArtemisInitializer.stopBroker();
+    public static void startBroker() throws Exception {
+        EmbeddedActiveMQ embeddedActiveMQ = new EmbeddedArtemisTestConfig().embeddedActiveMQ();
+        System.out.println("Broker started successfully!");
+        embeddedActiveMQ.start();
     }
 
     protected void triggerPostMessage() {
